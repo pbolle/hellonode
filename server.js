78d9f3f5583a3728404c5a60412a8a566370e395
jan-philipp.bolle <jan-philipp.bolle@aperto.de>
2016-03-30 20:22:30 +0200

    show envirement infos

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var os = require('os');
-
+var counter = 0;
 function getIPAddress() {
   var interfaces = os.networkInterfaces();
   var ips = "[";
@@ -17,9 +17,14 @@ function getIPAddress() {
 }
 
 var handleRequest = function(request, response) {
-  
+  console.log("request count:" + counter + " url:" + request.url);
+
   response.writeHead(200);
-  response.end("Hello World! hostname:"+os.hostname()+" ip:"+ getIPAddress());
+  response.end("Host info for:" + os.hostname() +
+                "\n ip:" + getIPAddress() +
+                "\n pid:" + process.pid +
+                "\n env:" + JSON.stringify(process.env, null, 2));
+  counter++;
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
